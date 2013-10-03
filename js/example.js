3626b8a99134ed4dca979ab30e24a99848beb83f
edh106 <hunnicutt.eli@gmail.com>
2013-10-03 11:47:34 -0500

    Really fixed tabs to 4 spaces

--- js/example.js
+++ js/example.js
@@ -8,11 +8,11 @@ $(document).delegate('#textbox', 'keydown', function(e) {
 
     // set textarea value to: text before caret + tab + text after caret
     $(this).val($(this).val().substring(0, start)
-                + "\t"
+                + "    "
                 + $(this).val().substring(end));
 
     // put caret at right position again
     $(this).get(0).selectionStart =
-    $(this).get(0).selectionEnd = start + 1;
+    $(this).get(0).selectionEnd = start + 4;
   }
 });
