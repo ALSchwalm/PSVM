7674300fc6440d4bd781cfb9cc96a5953ac5e2ff
Adam Schwalm <adamschwalm@gmail.com>
2013-10-05 23:13:43 -0500

    Added 404 on invalid username for profile page

--- backend/user.py
+++ backend/user.py
@@ -6,12 +6,12 @@ from login import *
 def profile(request):
     username = request.query_string.get("username", [""])[0]
 
-    if not username:
-        request.redirect_response("/404.html")
-    
     q = database.execute("SELECT * FROM users WHERE username = ?", (username,))
     user = q.fetchone()
 
+    if not user or not username:
+        return request.redirect_response("/404.html")
+    
     page = templates["profile"].format(
         username=username,
         email=user["email"])
