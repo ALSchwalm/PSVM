52bd54b63b3e8531158c04143dc61f4922ca58dc
edh106 <hunnicutt.eli@gmail.com>
2013-10-07 14:31:46 -0500

    Added javascript redirect after successful registration

---
+++ js/redirect.js
@@ -0,0 +1,8 @@
+var ind = document.URL.indexOf("?");
+var str = document.URL.substring(ind);
+console.log(str);
+if (str == "?prompt=success"){
+    setTimeout(function() {
+    document.location = "/login.html"; } , 500);
+    
+}
