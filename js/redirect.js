72fe2fb29c162be4f6f798a2c6f48a95df8f7b14
edh106 <hunnicutt.eli@gmail.com>
2013-10-07 14:42:28 -0500

    Removed console print

--- js/redirect.js
+++ js/redirect.js
@@ -1,6 +1,5 @@
 var ind = document.URL.indexOf("?");
 var str = document.URL.substring(ind);
-console.log(str);
 if (str == "?prompt=success"){
     setTimeout(function() {
     document.location = "/login.html"; } , 2000);
