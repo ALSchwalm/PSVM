77c3dbde7688aec25a4f939f9db67f2c06c7a134
Adam Schwalm <adamschwalm@gmail.com>
2013-10-09 14:33:07 -0500

    Added username to thread_link

--- backend/category.py
+++ backend/category.py
@@ -7,9 +7,12 @@ from thread import *
 def compose_category(category_id):
     q = database.execute("""
 
-    SELECT DISTINCT threads.thread_id, threads.title, categories.name
-    FROM categories LEFT OUTER JOIN threads
-    ON threads.category_id = categories.category_id
+    SELECT DISTINCT thread_id, title, name, username
+    FROM categories
+    LEFT OUTER JOIN threads
+    ON categories.category_id = threads.category_id
+    LEFT OUTER JOIN users
+    ON threads.op_id = users.user_id
     WHERE categories.category_id = ?
 
     """, (category_id,))
@@ -23,8 +26,12 @@ def compose_category(category_id):
     for thread in threads:
         thread_id = thread["thread_id"]
         title = thread["title"]
+        username = thread["username"]
         if thread_id:
-            thread_names += templates["thread_link"].format(thread_id=thread_id, title=title)
+            thread_names += templates["thread_link"].format(
+                thread_id=thread_id,
+                title=title,
+                username=username)
         else:
             thread_names += "None"
         thread_names += "<br>"
